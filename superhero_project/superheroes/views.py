4e21de72be964c485ecf9be8c58bce52a8f383b2
Megan Cindric <megancindric@gmail.com>
2021-09-02 17:13:51 -0500

    Scaffolding out edit & delete methods

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -32,7 +32,22 @@ def create(request):
         return render(request, 'superheroes/create.html')
 
 def delete(request):
+    #popup window asking for confirmation
+    #if yes, proceed, if no, do nothing
     pass
 
-def delete(request, hero_id):
-    pass
+def edit(request, hero_id):
+    #render view w/ hero details populating a form
+    #User can enter new values if they want
+    #On submission, will update
+    if request.method == "UPDATE":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        my_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        my_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/edit.html')
