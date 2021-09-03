434b016b07e02bb9937108dcc4910855d70f7395
Megan Cindric <megancindric@gmail.com>
2021-09-02 20:20:54 -0500

    Adding logic to edit existing heroes

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -39,15 +39,19 @@ def delete(request):
 def edit(request, hero_id):
     #render view w/ hero details populating a form
     #User can enter new values if they want
-    #On submission, will update
-    if request.method == "UPDATE":
-        name = request.POST.get('name')
-        alter_ego = request.POST.get('alter_ego')
-        primary_ability = request.POST.get('primary_ability')
-        secondary_ability = request.POST.get('secondary_ability')
-        catchphrase = request.POST.get('catchphrase')
-        my_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
-        my_hero.save()
+    #On submission, will grab existing superhero & update
+    if request.method == "POST":
+        existing_superhero = Superhero.objects.get(pk = hero_id)
+        existing_superhero.name = request.POST.get('name')
+        existing_superhero.alter_ego = request.POST.get('alter_ego')
+        existing_superhero.primary_ability = request.POST.get('primary_ability')
+        existing_superhero.secondary_ability = request.POST.get('secondary_ability')
+        existing_superhero.catchphrase = request.POST.get('catchphrase')
+        existing_superhero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
-        return render(request, 'superheroes/edit.html')
+        current_hero = Superhero.objects.get(pk=hero_id)
+        context = {
+        'current_hero': current_hero
+        }
+        return render(request, 'superheroes/edit.html', context)
